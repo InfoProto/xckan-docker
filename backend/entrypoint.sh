5b0e57b2de7f90a72c56d9e68ee0eec29b00fca6
Takeshi Sagara <sagara@info-proto.com>
2023-03-19 13:09:19 +0900

    Change Django model migration to run every time, even if Solr has already been initialized.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
+echo "Initialize..." > /ext/init.log
 
-# Check if solr collection is exsiting
-
+# Check if solr collection is exsiting.
 if [ ! -e "/ext/init.log" ]; then
-  echo "Initialize..." > /ext/init.log
-  echo "Making migrations." >> /ext/init.log
-  python manage.py makemigrations
-  echo "Migrating." >> /ext/init.log
-  python manage.py migrate
-  echo "Collecting static files." >> /ext/init.log
-  python manage.py collectstatic --noinput
-  echo "Creating superuser." >> /ext/init.log
-  python manage.py createsuperuser --noinput
-  echo "Creating collection." >> /ext/init.log
   sleep 5  # Wait for the solr server
   curl -X POST -H 'Content-type:application/json' \
     --data-binary @/app/ckan-xsearch/xckan-schema.json \
     ${XCKAN_SOLR}/schema
   echo `date` >> /ext/init.log
-else
-  echo "Collecting static files."
-  python manage.py collectstatic --noinput
 fi
 
+# Update Django models and static files.
+echo "Making migrations." >> /ext/init.log
+python manage.py makemigrations
+echo "Migrating." >> /ext/init.log
+python manage.py migrate
+echo "Collecting static files." >> /ext/init.log
+python manage.py collectstatic --noinput
+echo "Creating superuser." >> /ext/init.log
+python manage.py createsuperuser --noinput
+echo "Creating collection." >> /ext/init.log
+
+# Launch server.
 python manage.py runserver 0.0.0.0:5000
